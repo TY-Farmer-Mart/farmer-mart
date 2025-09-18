324c4c03825bf0d601fae1a1c4934e8a77d58d5a
Tamil <tamilselvanvenba@gmail.com>
2025-09-18 16:05:43 +0530

    feat:added input and button

--- src/types/authTypes.ts
+++ src/types/authTypes.ts
@@ -1,4 +1,27 @@
-export interface Login {
-    email:string,
-    password:number
+
+export interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  size?: "sm" | "md" | "lg";
+}
+
+
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?:
+    | "primary"
+    | "secondary"
+    | "outline"
+    | "ghost"
+    | "social"
+    | "socialapple";
+  size?: "sm" | "md" | "lg";
+  isLoading?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,13 @@
+import clsx from "clsx";
+import { ClassValue } from "clsx";
+
+
 export function truncateText(text: string, maxLength: number): string {
     if (text.length <= maxLength) return text;
     return text.slice(0, maxLength) + '...';
+}
+
+
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs);
 }
