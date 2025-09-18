711170a4890c0ab0a2bbc2e484a4b1b28028e7af
md rashid <md.rashid@qspiders.in>
2025-09-18 18:38:11 +0530

    footer text modified as per requirement.

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -25,30 +25,34 @@ const Footer = () => {
     <div className="w-[100%] mt-4">
       <div className="flex justify-between flex-col sm:flex-row bg-gray-200 px-10 py-2 items-center">
         <span className="text-xl text-blue-900">{footerHeaderText}</span>
-        <div className="flex gap-4 text-sm flex-col sm:flex-row">
+        <div className="flex gap-1 sm:gap-4 text-sm flex-col sm:flex-row">
           <div className="flex gap-2 items-center">
             <span>{goMobile}</span>
-            <span className="h-8 w-8 flex items-center justify-center  rounded-full cursor-pointer">
-              <Apple color="black" size={22} />
-            </span>
-            <span className="h-8 w-8 flex items-center justify-center  rounded-full cursor-pointer">
-              <Github color="black" size={22} />
-            </span>
-            <span className="h-8 w-8 flex items-center justify-center  rounded-full cursor-pointer">
-              <Smartphone color="black" size={22} />
-            </span>
+            <div className="flex">
+              <span className="h-8 w-8 flex items-center justify-center  rounded-full cursor-pointer">
+                <Apple color="black" size={22} />
+              </span>
+              <span className="h-8 w-8 flex items-center justify-center  rounded-full cursor-pointer">
+                <Github color="black" size={22} />
+              </span>
+              <span className="h-8 w-8 flex items-center justify-center  rounded-full cursor-pointer">
+                <Smartphone color="black" size={22} />
+              </span>
+            </div>
           </div>
           <div className="flex gap-2 items-center">
             <span>{followUsOn}</span>
-            <span className="h-8 w-8 flex items-center justify-center bg-blue-950 rounded-full cursor-pointer">
-              <Facebook color="white" size={22} />
-            </span>
-            <span className="h-8 w-8 flex items-center justify-center bg-blue-950 rounded-full cursor-pointer">
-              <Twitter color="white" size={22} />
-            </span>
-            <span className="h-8 w-8 flex items-center justify-center bg-blue-950 rounded-full cursor-pointer">
-              <Linkedin color="white" size={22} />
-            </span>
+            <div className="flex gap-2">
+              <span className="h-8 w-8 flex items-center justify-center bg-blue-950 rounded-full cursor-pointer">
+                <Facebook color="white" size={22} />
+              </span>
+              <span className="h-8 w-8 flex items-center justify-center bg-blue-950 rounded-full cursor-pointer">
+                <Twitter color="white" size={22} />
+              </span>
+              <span className="h-8 w-8 flex items-center justify-center bg-blue-950 rounded-full cursor-pointer">
+                <Linkedin color="white" size={22} />
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -56,21 +60,32 @@ const Footer = () => {
         <div className="">
           {aboutUs.map((val, ind) => (
             <div>
-              <p key={ind}>{val}</p>
+              <p key={ind} className="cursor-pointer">
+                {val}
+              </p>
             </div>
           ))}
         </div>
         <div>
           {jobCareers.map((val, ind) => (
             <div>
-              <p key={ind}>{val}</p>
+              <p key={ind} className="cursor-pointer">
+                {val}
+              </p>
             </div>
           ))}
         </div>
         <div>
           {buyToolkit.map((val, ind) => (
             <div>
-              <p key={ind} className={ind === 0 ? "text-xl text-black" : "sm"}>
+              <p
+                key={ind}
+                className={
+                  ind === 0
+                    ? "text-xl text-black cursor-pointer"
+                    : "text-sm cursor-pointer"
+                }
+              >
                 {val}
               </p>
             </div>
@@ -81,7 +96,11 @@ const Footer = () => {
             <div>
               <p
                 key={ind}
-                className={ind === 0 ? "text-xl text-black" : "text-sm"}
+                className={
+                  ind === 0
+                    ? "text-xl text-black cursor-pointer"
+                    : "text-sm cursor-pointer"
+                }
               >
                 {val}
               </p>
@@ -93,7 +112,11 @@ const Footer = () => {
             <div>
               <p
                 key={ind}
-                className={ind === 0 ? "text-xl text-black" : "text-sm"}
+                className={
+                  ind === 0
+                    ? "text-xl text-black cursor-pointer"
+                    : "text-sm cursor-pointer"
+                }
               >
                 {val}
               </p>
@@ -103,7 +126,7 @@ const Footer = () => {
       </div>
       <div className="flex justify-between flex-col sm:flex-row gap-4 sm:gap-0 text-gray-700 text-sm pt-2">
         <p>{copyRight}</p>
-        <p>{termsUse}</p>
+        <p className="pointer cursor-pointer">{termsUse}</p>
       </div>
     </div>
   );
