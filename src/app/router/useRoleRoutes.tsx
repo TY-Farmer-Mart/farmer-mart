7996ea0c950970531c383f56e38c655adc4e9163
jaya kiran <jayakiransudanagunta@gmail.com>
2025-09-22 10:48:21 +0530

    feat:created search-page-layout

--- src/app/router/useRoleRoutes.tsx
+++ src/app/router/useRoleRoutes.tsx
@@ -2,6 +2,7 @@
 import type { RouteObject } from "react-router-dom";
 import HomeLayout from "../layouts/homeLayouts";
 import UserRoutes from "./user/userRoutes";
+import SearchLayout from "../layouts/SearchLayout";
 
 export const useRoleRoutes = (): RouteObject[] => {
 
@@ -11,6 +12,10 @@ export const useRoleRoutes = (): RouteObject[] => {
             {
                 element: <HomeLayout />,
                 children: UserRoutes,
+            },
+            {
+                element:<SearchLayout />,
+                path:"/searchlayout",            
             },
         ];
     }
