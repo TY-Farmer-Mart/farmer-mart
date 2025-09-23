9180abfe322caa5c037a69ca78c0be3412f8eaa9
Sakshi-Kumari07 <sakshi.k@testyantra.in>
2025-09-23 15:48:15 +0530

    fix : resolve conflicts

--- src/features/auth/Authwrapper.tsx
+++ src/features/auth/Authwrapper.tsx
@@ -3,7 +3,7 @@ import { AuthLayoutProps } from "@/types/authTypes";
 import farmerLogo from "@assets/images/farmerLogo.png";
 import farmerBg from "@assets/images/farmerBg.png";
 import React from "react";
- 
+
 export const AuthWrapper: React.FC<AuthLayoutProps> = ({ children, title }) => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row relative">
@@ -13,9 +13,9 @@ export const AuthWrapper: React.FC<AuthLayoutProps> = ({ children, title }) => {
           alt="Farmer Background"
           className="absolute inset-0 w-full h-full object-cover z-0"
         />
- 
+
         <div className="absolute inset-0 bg-black-900 bg-opacity-30 z-10" />
- 
+
         <svg
           className="absolute top-0 -right-11 lg:-right-6 h-full z-20"
           viewBox="0 0 200 800"
@@ -23,7 +23,7 @@ export const AuthWrapper: React.FC<AuthLayoutProps> = ({ children, title }) => {
         >
           <path d="M0,0 Q200,400 0,800 L200,800 L200,0 Z" fill="white" />
         </svg>
- 
+
         <div className="absolute inset-0 z-30 flex flex-col justify-between pl-6 lg:px-12 py-8 text-white">
           <div>
             <div className="flex items-center space-x-3">
@@ -34,10 +34,10 @@ export const AuthWrapper: React.FC<AuthLayoutProps> = ({ children, title }) => {
               />
               <h1 className="text-xl font-bold ">Farmer Mart</h1>
             </div>
- 
+
             <div className="w-[60%] pt-5">
               <h2 className="text-2xl lg:text-3xl font-bold py-2">
-                {AUTH_TEXT.AUTH_HEADER}
+                {AUTH_TEXT.AUTH_HEADER}{" "}
                 <span className="italic font-normal">
                   {AUTH_TEXT.AUTH_SUBHEADING}
                 </span>
@@ -45,13 +45,13 @@ export const AuthWrapper: React.FC<AuthLayoutProps> = ({ children, title }) => {
               </h2>
             </div>
           </div>
- 
+
           <p className="mb-5 max-w-md font-normal text-sm lg:text-xl leading-[1.5rem] whitespace-pre-line">
             {AUTH_TEXT.AUTH_FOOTER}
           </p>
         </div>
       </div>
- 
+
       <div className="flex w-full lg:w-[50%] justify-center items-center px-6  lg:px-6  bg-white z-20 relative">
         <div className="w-full max-w-sm lg:max-w-md space-y-6 py-12">
           {title && (
@@ -65,5 +65,3 @@ export const AuthWrapper: React.FC<AuthLayoutProps> = ({ children, title }) => {
     </div>
   );
 };
- 
- 
