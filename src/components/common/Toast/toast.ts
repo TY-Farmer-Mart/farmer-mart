65be94dd50139f2cda6e2e7c9e4eaf943d7a8296
Tamil <tamilselvanvenba@gmail.com>
2025-09-29 19:44:11 +0530

    feat: add toast

--- src/components/common/Toast/toast.ts
+++ src/components/common/Toast/toast.ts
@@ -3,5 +3,5 @@ import toast from "react-hot-toast";
 
 export const showSuccess = (message: string) => toast.success(message);
 export const showError = (message: string) => toast.error(message);
-export const showLoading = (message: string) => toast.loading(message);
+export const showLoading = (message: string, p0: { duration: number; }) => toast.loading(message);
 export const dismissToast = (id: string) => toast.dismiss(id);
